5a54ace6fc1fc20a862a304954a2864986c620b9
angikkate <angikkate@gmail.com>
2025-06-12 16:33:08 +0300

    Create function

--- task_1.py
+++ task_1.py
@@ -1,4 +1,14 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    
+
+    try:
+        datetime_object = datetime.strptime(date, "%Y-%m-%d") # Create datetime object
+        current_date = datetime.today()  # To get current date
+        delta_day = current_date - datetime_object  # Difference between two dates
+
+        return delta_day.days
+    except ValueError: 
+        return "Invalid date format. Enter date in 'YYYY-MM-DD' format."
+
+get_days_from_today('2024-12-05')
